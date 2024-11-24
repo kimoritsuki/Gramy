bcc47a93ebe59c30895fdd2eed3499437a02d98f
Hugo <hugo.lacourr@gmail.com>
2024-11-24 12:55:36 +0100

    Initial Commit

---
+++ Gramy/GramyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GramyApp.swift
+//  Gramy
+//
+//  Created by Lacour Hugo on 24/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct GramyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
